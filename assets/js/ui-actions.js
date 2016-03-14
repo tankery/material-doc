02f33857ade6484b730fdaf171bd851af6deb348
Tankery Chen <tankery.chen@gmail.com>
2016-03-15 06:47:05 +0800

    Built base structure of the site.

---
+++ assets/js/ui-actions.js
@@ -0,0 +1,70 @@
+(function($){
+  $(function(){
+
+    var window_width = $(window).width();
+
+
+    // Floating-Fixed table of contents
+    setTimeout(function() {
+      var tocWrapperHeight = 260; // Max height of ads.
+      var tocHeight = $('.toc-wrapper .table-of-contents').length ? $('.toc-wrapper .table-of-contents').height() : 0;
+      var socialHeight = 95; // Height of unloaded social media in footer.
+      var bottomOffset = $('footer').offset().top - socialHeight - tocHeight - tocWrapperHeight;
+
+      if ($('nav').length) {
+        $('.toc-wrapper').pushpin({
+          top: $('nav').height(),
+          bottom: bottomOffset
+        });
+      }
+      else if ($('#index-banner').length) {
+        $('.toc-wrapper').pushpin({
+          top: $('#index-banner').height(),
+          bottom: bottomOffset
+        });
+      }
+      else {
+        $('.toc-wrapper').pushpin({
+          top: 0,
+          bottom: bottomOffset
+        });
+      }
+    }, 100);
+
+    // Detect touch screen and enable scrollbar if necessary
+    function is_touch_device() {
+      try {
+        document.createEvent("TouchEvent");
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+    if (is_touch_device()) {
+      $('#nav-mobile').css({ overflow: 'auto'});
+    }
+
+    // Set checkbox on forms.html to indeterminate
+    var indeterminateCheckbox = document.getElementById('indeterminate-checkbox');
+    if (indeterminateCheckbox !== null)
+      indeterminateCheckbox.indeterminate = true;
+
+
+    // Plugin initialization
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.carousel').carousel();
+    $('.slider').slider({full_width: true});
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.scrollspy').scrollSpy();
+    $('.datepicker').pickadate({selectYears: 20});
+    $('select').not('.disabled').material_select();
+
+    // Initialize collapse button
+    $(".button-collapse").sideNav({'edge': 'left'});
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    // $('.collapsible').collapsible();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
