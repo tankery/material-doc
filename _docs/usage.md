0eeab2ff85309dbbc44a648468a8cec7a93191de
Tankery Chen <tankery.chen@gmail.com>
2016-03-20 14:10:30 +0800

    Add Table of Contents widget in page.

---
+++ _docs/usage.md
@@ -0,0 +1,27 @@
+---
+layout: doc
+title: How to Use
+permalink: /docs/usage/
+---
+
+Here is a introduction for how to use the [Material Doc][material-doc].
+
+## Installation
+
+[Material Doc][material-doc] is a Jekyll powered site, with can be host directly on [GitHub Pages][github-pages].
+
+Al things you need to do is to [fork][material-doc-code] this Project. Then you can visit your own site on `http://<username>.github.io/material-doc/`.
+
+Anything you can on branch `gh-pages` will be displayed on your site.
+
+For more details about site hosting (eg. Change site name, use your own domain, etc.) visit [GitHub Pages][github-pages].
+
+## Writing docs
+
+## Custom Theme
+
+## Develop
+
+[material-doc]: http://tankery.me/material-doc/
+[material-doc-code]: https://github.com/tankery/material-doc
+[github-pages]: http://tankery.me/material-doc/
--- assets/js/ui-actions.js
+++ assets/js/ui-actions.js
@@ -4,33 +4,6 @@
     var window_width = $(window).width();
 
 
-    // Floating-Fixed table of contents
-    setTimeout(function() {
-      var tocWrapperHeight = 260; // Max height of ads.
-      var tocHeight = $('.toc-wrapper .table-of-contents').length ? $('.toc-wrapper .table-of-contents').height() : 0;
-      var socialHeight = 95; // Height of unloaded social media in footer.
-      var bottomOffset = $('footer').offset().top - socialHeight - tocHeight - tocWrapperHeight;
-
-      if ($('nav').length) {
-        $('.toc-wrapper').pushpin({
-          top: $('nav').height(),
-          bottom: bottomOffset
-        });
-      }
-      else if ($('#index-banner').length) {
-        $('.toc-wrapper').pushpin({
-          top: $('#index-banner').height(),
-          bottom: bottomOffset
-        });
-      }
-      else {
-        $('.toc-wrapper').pushpin({
-          top: 0,
-          bottom: bottomOffset
-        });
-      }
-    }, 100);
-
     // Detect touch screen and enable scrollbar if necessary
     function is_touch_device() {
       try {
@@ -44,26 +17,24 @@
       $('#nav-mobile').css({ overflow: 'auto'});
     }
 
-    // Set checkbox on forms.html to indeterminate
-    var indeterminateCheckbox = document.getElementById('indeterminate-checkbox');
-    if (indeterminateCheckbox !== null)
-      indeterminateCheckbox.indeterminate = true;
+    // Floating-Fixed table of contents
+    $('.post-container .table-of-contents').toc({
+      title: '',
+      listType: 'ul',
+      headers: '.post-content h2'
+    });
 
 
     // Plugin initialization
-    $('.carousel.carousel-slider').carousel({full_width: true});
-    $('.carousel').carousel();
-    $('.slider').slider({full_width: true});
-    $('.parallax').parallax();
-    $('.modal-trigger').leanModal();
-    $('.scrollspy').scrollSpy();
-    $('.datepicker').pickadate({selectYears: 20});
-    $('select').not('.disabled').material_select();
 
     // Initialize collapse button
     $(".button-collapse").sideNav({'edge': 'left'});
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    // $('.collapsible').collapsible();
+
+    // Enable Scroll-spy on h2 in post-content
+    $('.post-content h2').scrollSpy();
+
+    // Pin toc to top.
+    $('.post-container .table-of-contents').pushpin({ top: $('.table-of-contents').offset().top });
 
 
   }); // end of document ready
